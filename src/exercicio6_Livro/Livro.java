9f1820f0bdb6a1d31ce23f72e99bf8ca8fc13b65
comcod <netosilvaneto6@gmail.com>
2025-04-04 19:29:18 -0300

    Modificações extras

--- src/exercicio6_Livro/Livro.java
+++ src/exercicio6_Livro/Livro.java
@@ -1,44 +1,33 @@
 package exercicio6_Livro;
+
 import java.util.Scanner;
 
 public class Livro {
 	private String titulo;
 	private String autor;
 	private double preco;
-	
+
 	Scanner input = new Scanner(System.in);
-	/*
-	 * Construtor
-	 * Livro (String titulo, String autor, double preco) {
-	 * 	this.titulo = titulo;
-	 * 	this.autor = autor;
-	 * 	this.preco = preco;
-	 * }
-	 */
-	
-	void setTitulo() {
-		System.out.println("Insira o titulo da obra: ");
-		String newTitulo = input.nextLine();
-		this.titulo = newTitulo;
+
+	Livro(String titulo, String autor, double preco) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.preco = preco;
 	}
-	
-	void setAutor() {
-		System.out.println("Insira o autor da obra: ");
-		String newAutor = input.nextLine();
-		this.autor = newAutor;
+
+	void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
-	
-	void setPreco() {
-		System.out.println("Insira o preço da obra: ");
-		double newPreco = input.nextDouble();
-		this.preco = newPreco;
+
+	void setAutor(String autor) {
+		this.autor = autor;
 	}
-	
-	void seeAll () {
-		System.out.println("\nAs informações da obra " + titulo + " são:\n");
-		System.out.println(titulo);
-		System.out.println(autor);
-		System.out.println(preco);
-		System.out.println("\n\n");
+
+	void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	void exibirInfo() {
+		System.out.println("Autor: " + this.autor + "\nPreço: " + this.preco + "\nTitulo: " + this.titulo);
 	}
 }
--- src/exercicio6_Livro/Main.java
+++ src/exercicio6_Livro/Main.java
@@ -1,24 +1,94 @@
 package exercicio6_Livro;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Livro iceAndFire = new Livro();
-		Livro laranjinha = new Livro();
-		
-	
-		iceAndFire.setTitulo();
-		iceAndFire.setAutor();
-		iceAndFire.setPreco();
-		
-		iceAndFire.seeAll();
-		
-		laranjinha.setTitulo();
-		laranjinha.setAutor();
-		laranjinha.setPreco();
-		
-		laranjinha.seeAll();
-		
+		Scanner input = new Scanner(System.in);
+		ArrayList<Livro> livros = new ArrayList<>();
+
+		while (true) {
+			System.out.println("[1] Cadastrar Livro\n[2] Listar Livros\n[3] Editar Livros\n[4] Sair\n\nEscolha uma opção");
+			int escolha = input.nextInt();
+
+			switch (escolha) {
+			case 1:
+				System.out.println("Digite o autor do livro:");
+				String autor = input.nextLine();
+				
+				System.out.println("Digite o nome do livro:");
+				String titulo = input.nextLine();
+				
+				System.out.println("Digite o valor do livro:");
+				double preco = input.nextDouble();
+
+				livros.add(new Livro(titulo, autor, preco));
+
+				System.out.println("Livro cadastrado.\n");
+				break;
+			case 2:
+				if (livros.isEmpty()) {
+					System.out.println("Não há livros cadastrados!");
+
+				} else {
+					System.out.println("Livros cadastrados:\n");
+					for (int i = 0; i < livros.size(); i++) {
+						System.out.println("livro " +  i + 1  + ":");
+						livros.get(i).exibirInfo();
+					}
+				}
+				break;
+
+			case 3:
+				if (livros.isEmpty()) {
+					System.out.println("Não há livros para editar informações.\n);");
+					break;
+				} else {
+					System.out.println("Digite o numero do livro: ");
+					int index = input.nextInt();
+
+					if (index >= 0 && index < livros.size()) {
+						Livro livro = livros.get(index);
+						System.out.println("editando livro:\n");
+						livro.exibirInfo();
+						System.out.println("\n");
+
+						System.out.println("novo titulo(ou ENTER para não mudar): ");
+						String newTitulo = input.nextLine();
+
+						if (!newTitulo.isEmpty())
+							livro.setTitulo(newTitulo);
+
+						System.out.println("Novo autor(ou enter para não mudar): ");
+						String newAutor = input.nextLine();
+
+						if (!newAutor.isEmpty())
+							livro.setAutor(newAutor);
+
+						System.out.println("Novo preço(ou '-1' para não mudar): ");
+						double newPreco = input.nextDouble();
+						if (newPreco >= 0)
+							livro.setPreco(newPreco);
+
+						System.out.println("Atualizado!");
+					} else {
+						System.out.println("indice errado.");
+						break;
+					}
+					break;
+				}
+			case 4:
+				System.out.println("Saindo...");
+				input.close();
+				return;
+
+			default:
+				System.out.println("Opção invalida.");
+			}
+
+		}
 	}
 
 }
